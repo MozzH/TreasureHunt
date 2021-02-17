c70e41653719c6761e0709ab933d0daedb239fe0
LefterisT <76411367+LefterisT@users.noreply.github.com>
2021-02-17 18:17:54 +0200

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -2,7 +2,7 @@
 <html>
 <head>
 <script>
-    <!-- we are setting new cookies by gettig the name the time should expire and the value -->
+    <!-- we are setting new cookies by gettig the name,the time should expire and the value -->
 function setCookie(cname,cvalue,exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
