47973349c739bca2f50c3f6fb6055abdfd67521f
LefterisT <76411367+LefterisT@users.noreply.github.com>
2021-02-17 17:50:09 +0200

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,11 +1,25 @@
-function getGeolocation()
-{
-    if(navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(getGeolocation);
-    }
-    if(!navigator.geolocation)
-    {
-        alert("Your Location couldn't be fetched");
-    }
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>geolocation.</p>
+
+<script>
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
+
+function showPosition(position) {
+console.log("Latitude: " + position.coords.latitude + ", Longitude: " +
+position.coords.longitude);
+}
+
+getLocation();
+</script>
+
+</body>
+</html>
