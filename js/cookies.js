120c8c63fd9e332b9b99c15d773d8799916f6db2
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2021-03-03 15:59:08 +0200

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,30 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script>
-    <!-- we are setting new cookies by gettig the name,the time should expire and the value -->
-function setCookie(cname,cvalue,exdays) {
+//setting new cookies by gettig the name,the time should expire and the value
+function setCookie(cname,cvalue,exdays)
+{
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-<!--returns the value of the cookie-->
-function getCookie(cname) {
+
+//returns the value of the cookie
+function getCookie(cname)
+{
   var name = cname + "=";
   var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
+    
+  for(var i = 0; i < ca.length; i++)
+  {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == ' ') 
+    {
       c = c.substring(1);
     }
-    if (c.indexOf(name) == 0) {
+      
+    if (c.indexOf(name) == 0) 
+    {
       return c.substring(name.length, c.length);
     }
   }
   return "";
 }
-}
-</script>
-</head>
-</html>
