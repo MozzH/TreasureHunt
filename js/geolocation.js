acbdf9a75e3e83036d6e38235ed66a5e91a28089
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2021-03-03 15:57:00 +0200

    code fix

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,27 +1,21 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<p>geolocation.</p>
-
-<script>
-  <!-- getting the position of the user -->
-function getLocation() {
-  if (navigator.geolocation) {
+//getting the position of the user
+function getLocation()
+{
+  if (navigator.geolocation)
+  {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
+  } 
+  else 
+  { 
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
-<!-- showing the postition of the user -->
-function showPosition(position) {
- <!-- we are showing the location of the user on the console for debugging issues -->
-console.log("Latitude: " + position.coords.latitude + ", Longitude: " +
-position.coords.longitude);
+
+//showing the postition of the user
+function showPosition(position) 
+{
+  //showing the location of the user on the console for debugging issues
+  console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
 }
 
 getLocation();
-</script>
-
-</body>
-</html>
