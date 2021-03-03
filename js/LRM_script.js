8c70d83c6308bf8f8cd796878d32dab09dcd9059
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2021-03-03 16:46:22 +0200

    Add files via upload

--- js/LRM_script.js
+++ js/LRM_script.js
@@ -13,9 +13,22 @@ const score_api = url_api + "/score";
 const leaderboard = url_api + "/leaderboard";
 
 //Question type
-const qtype_choice = "MCQ";        //Multiple Choice
+const qtype_choice = "MCQ";     //Multiple Choice
 const qtype_text = "";          //Text
 const qtype_num = "";           //Numeric
 const qtype_int = "INTEGER";    //Integer
 const qtype_bool = "BOOLEAN";   //Boolean
 
+//check for empty answer field
+function checkField()
+{
+    //if field empty, return false; otherwise true
+    if (document.getElementById('').value == "")
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
