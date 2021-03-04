9e09aa567037d521115a4d6330adf56a001d25d2
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2021-03-04 22:58:16 +0200

    Add files via upload

---
+++ LRM_script.js
@@ -0,0 +1,40 @@
+//URL of API
+const url_api = "https://www.codecyprus.org/th/api";
+
+//URL of API + API function calling
+const selection_api = url_api + "/list";
+const start_api = url_api + "/start";
+
+const question_api = url_api + "/question";
+const answer_api = url_api + "/answer";
+const skip_api = url_api + "/skip";
+
+const score_api = url_api + "/score";
+const leaderboard = url_api + "/leaderboard";
+
+//Question type
+const qtype_choice = "MCQ";     //Multiple Choice
+const qtype_text = "";          //Text
+const qtype_num = "";           //Numeric
+const qtype_int = "INTEGER";    //Integer
+const qtype_bool = "BOOLEAN";   //Boolean
+
+//check for empty answer field
+function checkField()
+{
+    //if field empty, return false; otherwise true
+    if (document.getElementById('').value == "")
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+//insert function to values from GET
+function getGet()
+{
+
+}
---
+++ cookies.js
@@ -0,0 +1,63 @@
+/* naming functions:
+*   e.g. setCookie
+*   verb + Cookie
+*   set + Cookie
+*/
+//cookies
+const playerName_cookie;
+const session_cookie;
+
+//setting new cookies by getting the name,the time should expire and the value
+function setCookie(cookieName, cookieValue, expirationDays)
+{
+  var date = new Date();
+  date.setTime(date.getTime() + (expirationDays*24*60*60*1000));
+  var expires = "expires=" + date.toGMTString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+//returns the value of the cookie
+function getCookie(cookieName)
+{
+  var name = cookieName + "=";
+  var ca = document.cookie.split(';');
+    
+  for(var i = 0; i < ca.length; i++)
+  {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') 
+    {
+      c = c.substring(1);
+    }
+      
+    if (c.indexOf(name) == 0) 
+    {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(cookieName)
+{
+  if (getCookie(cookieName) == "")
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+function deleteCookie(cookieName)
+{
+  let cookieValue = "";
+  var date = new Date();
+  var time = date.getTime()
+
+  date.setTime(time);
+
+  //find a way to delete stuff
+
+}
