7e6239884e6d485a149e95bdff6009fd28c3f019
LefterisT <76411367+LefterisT@users.noreply.github.com>
2021-03-18 12:36:12 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,5 @@
-
+const api = "https://codecyprus.org/th/api";
+const test_api = "https://codecyprus.org/th/test-api";
+const location_api = url_api + "/location";
+const score_api = url_api + "/score";
+const leaderboard_api = url_api + "/leaderboard";
